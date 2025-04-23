e6d0680c9e44548685972c5dace592d2d0429886
Margarita <m.v.herasymova@student.khai.edu>
2025-04-23 20:01:32 +0300

    Practice

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,11 @@ use App\Http\Controllers\GuestbookController;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-
 Route::get('/register', [RegisterController::class, 'show'])->name('register');
 Route::post('/register', [RegisterController::class, 'register'])->name('register.submit');
-
-// 🔹 Форма входу (GET) та обробка входу (POST)
 Route::get('/login', [LoginController::class, 'show'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
-
 Route::get('/logout', [LogoutController::class, 'logout'])->name('logout');
-
 Route::get('/guestbook', [GuestbookController::class, 'index'])->name('guestbook');
 Route::post('/guestbook', [GuestbookController::class, 'store'])->name('guestbook.store');
-
 Route::get('/admin', [AdminController::class, 'index'])->name('admin');
